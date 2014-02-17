1a66013764a7de02c54a308a395e406a753495f1
Mani Nilchiani <mani.art@gmail.com>
2014-02-17 11:43:14 -0500

    added complete js

--- class-code/session-3/main.js
+++ class-code/session-3/main.js
@@ -1,8 +1,83 @@
 var app = (function(w, d) {
 
-	
+	var DOM = {
+		'write-note-form' : d.querySelector('.write-note-form'),
+		'write-note' : d.querySelector('.write-note'),
+		'submit-note' :  d.querySelector('.submit-note'),
+		'notes' : d.querySelector('.notes')
+	};
+
+	var sanitize = function(string) {
+		return string.replace(/(^\s+|\s+$)/g,'');
+	};
+	var attachEvents = function() {
+		DOM['submit-note'].addEventListener('click', function(e) {
+			e.preventDefault();
+			notes.push(new Note(sanitize(DOM['write-note'].value), DOM['notes']).init());
+			DOM['write-note'].value = '';
+
+		});
+	};
+
+	var Note = function(text, list) {
+		this.text = text;
+		this.list = list;
+		this.listItem = d.createElement('li');
+		this.paragraph = d.createElement('p');
+		this.actions = d.createElement('ul');
+		this.likeButton = d.createElement('li');
+		this.removeButton = d.createElement('li');
+		this.liked = false;
+		this.like = function() {
+			
+			
+		};
+		this.remove = function() {
+			
+			
+		};
+		this.attachEvents = function() {
+			this.likeButton.addEventListener('click', function() {
+				// implement LIKE functionality	
+			});
+			this.removeButton.addEventListener('click', function() {
+				// implement remove functionality	
+			});
+		};
+		this.init = function() {
+			this.listItem.className += 'note';
+			this.paragraph.innerHTML = this.text;
+			this.listItem.appendChild(this.paragraph);
+			this.actions.className += 'actions';
+			this.likeButton.className += 'like icon-heart';
+			this.removeButton.className += 'remove icon-cancel';
+			this.actions.appendChild(this.removeButton);
+			this.actions.appendChild(this.likeButton);
+			this.listItem.appendChild(this.paragraph);
+			this.listItem.appendChild(this.actions);
+			this.list.appendChild(this.listItem);
+			this.attachEvents();
+			return this;
+		}
+
+	};
+
+	var notes = [];
+
+	var init = function() {
+		console.log('App init');
+		attachEvents();
+	};
+
+	return {
+		DOM : DOM,
+		attachEvents : attachEvents,
+		init : init,
+		notes : notes
+	};
 
 
 })(window, document);
 
+window.addEventListener('DOMContentLoaded', app.init);
 
