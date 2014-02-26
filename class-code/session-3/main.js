084c36d868948f86851d4c8c43ac75440f12715a
Mani Nilchiani <mani.art@gmail.com>
2014-02-26 02:07:18 -0500

    added syllabus .pdf

--- class-code/session-3/main.js
+++ class-code/session-3/main.js
@@ -1,1 +1,28 @@
-// js goes here
+
+var dude = (function() {
+  	
+  var name = 'Justin Charles';
+  var getName = function() {
+  	return name;
+  };
+  var setName = function(newName) {
+  	name = newName;
+  };
+  
+  return { 
+    getName : getName,
+    setName : setName
+  };  
+  
+})();
+
+var shouter = (function() {
+  var shout = function(name) {
+     alert(name);
+  };
+  
+  return {
+    shout : shout
+  };
+  
+})();
