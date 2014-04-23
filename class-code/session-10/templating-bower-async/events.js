33141fcc056b7c2fb518809170fee2bbb902ce09
Mani Nilchiani <mani.art@gmail.com>
2014-04-23 12:30:00 -0400

    added comments for templating bower asnyc

--- class-code/session-10/templating-bower-async/events.js
+++ class-code/session-10/templating-bower-async/events.js
@@ -1,13 +1,63 @@
+/* call in the app module we call "kitten" */
 var kittens = kittens || {};
 
+/* implement our events module under the "kitten" namespace */
 kittens.events = (function(w, d, $) {
+    /* 
+        publish events.
+        here's what it's all about:
+            1 - publish an event with a name. This can be whatever string you want.
+                Pass whatever data you want along with it.
+                Here is an example of passing a config object with the event.
+                e.g: 
 
+                    events.publish('this:is:the:name:of:my:event', { sky : 'blue', banana : 'yellow' });
+        
+        This is a fantastic way to allow our app modules and pieces to talk to one abother.
+        This is just a wrapper function for convenience, around jQuery's "trigger" eventing method.
+        More on wrapper functions: http://blakeembrey.com/articles/wrapping-javascript-functions/
+        More on jQuery trigger: http://api.jquery.com/trigger/
+     */
 	var publish = function (name, o) {
        
         console.log("EVENT [" + name + "]", o);
         $(document).trigger(name, [o]);
     
     };
+
+    /* 
+        subscribe to events. 
+        here's what it's all about:
+            2 - subscribe to events and invoke callback functions. the data that you passed along using "publish"
+                will be magically passed into your callback function. This is a vert powerful feature:
+                1st argument to "subscribe" method is the name of event.
+                2nd argyment to "subscribe" method is the callback function.
+                e.g:
+                
+                a)
+
+                    events.subscribe('this:is:the:name:of:my:event', function(data) {
+                       console.log(data); // puts ->  { sky : 'blue', banana : 'yellow' }
+                    });
+
+                Instead of writing the callback function as an anonymous funtion (nameless function) 
+                as the second argument of "subscribe", you can also define your function elsewhere and just call it
+                in your "subscribe":
+                
+                b)
+
+                    var callback = function() {
+                        console.log(data); // puts ->  { sky : 'blue', banana : 'yellow' }
+                    };
+                    events.subscribe('this:is:the:name:of:my:event', callback);
+
+                b and a are identical. mostly stylistic choice. I like b better. neater. up to you.
+
+        This is a fantastic way to allow our app modules and pieces to talk to one abother.
+        This is just a wrapper function for convenience, around jQuery's "on" eventing method.
+        More on wrapper functions: http://blakeembrey.com/articles/wrapping-javascript-functions/
+        More on jQuery on: https://api.jquery.com/on/
+     */
 
     var subscribe = function (name, callback) {
         
@@ -17,6 +67,7 @@ kittens.events = (function(w, d, $) {
 
     };
 
+    /* return public methods to be consumed by other modules (main.js) */
     return {
     	publish : publish,
     	subscribe : subscribe
--- class-code/session-10/templating-bower-async/main.js
+++ class-code/session-10/templating-bower-async/main.js
@@ -8,13 +8,19 @@ kittens.main = (function(w, d, $, _) {
 	tpl,
 	renderGif;
 
-
+	/* 
+		Using jQuery get to asynchronously
+		more on jQuery $.get : https://api.jquery.com/jQuery.get/ 
+		checkout "publish" implementation in events.js
+	*/
 	fetchTemplate = function() {
 		$.get('./tpl.html', function(tpl) {
 			kittens.events.publish('load-complete:template', tpl);
+			// got to events.js to see how this works.
 		});
 	};
 
+	/* this is identical to the example in templating-hower folder */
 	fetchData = function() {
 		$.getJSON('http://api.giphy.com/v1/gifs/search?q=kitten&api_key=dc6zaTOxFJmzC&limit=20', function(result) {
 			console.log('data received ', result.data);
@@ -22,7 +28,14 @@ kittens.main = (function(w, d, $, _) {
 		});
 	};
 
+	/*
+		because we load in our template asynchronously, we need to wait for this AJAX request 
+		to be completed, make sure that we have our template ready before we render.
 
+		OUR TEMPLATE NOW LIVES IN ITS OWN FILE: TPL.HTML ----- LOOK AT IT!
+		
+		checkout "subscribe" implementation in events.js
+	*/
 	render = function() {
 		kittens.events.subscribe('load-complete:template', function(tpl) {
 			renderGif = _.template(tpl);
